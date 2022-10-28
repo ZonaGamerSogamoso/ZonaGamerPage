a2f4aed10755d0122b7584d0bbf5890e0c4da5fb
Andrés <sergio.cruz02@uptc.edu.co>
2022-10-28 02:14:16 -0500

    Modify colors items

--- src/pages/About/components/SectionMap.jsx
+++ src/pages/About/components/SectionMap.jsx
@@ -4,7 +4,7 @@ export const SectionMap = () => {
         <div className="lg:pt-24 pt-6 w-full md:w-4/12 px-4 text-center">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
             <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-blue-500">
+              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-cyan-500">
                 <i className="fas fa-map"></i>
               </div>
               <h6 className="text-xl font-semibold">Ubicación</h6>
@@ -18,7 +18,7 @@ export const SectionMap = () => {
         <div className="pt-12 w-full md:w-4/12 px-4 text-center">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
             <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lime-600">
                 <i className="fas fa-map-pin"></i>
               </div>
               <h6 className="text-xl font-semibold">Dirección</h6>
@@ -32,7 +32,7 @@ export const SectionMap = () => {
         <div className="pt-24 w-full md:w-4/12 px-4 text-center">
           <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
             <div className="px-4 py-5 flex-auto">
-              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+              <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-700">
                 <i className="fas fa-clock"></i>
               </div>
               <h6 className="text-xl font-semibold">Horarios</h6>
