5753065c0bad06e07219a8173704ea6f1a9afffa
Andrés <sergio.cruz02@uptc.edu.co>
2022-10-27 23:36:19 -0500

    New Page About

--- src/pages/About/views/AboutPage.jsx
+++ src/pages/About/views/AboutPage.jsx
@@ -1,8 +1,17 @@
+import { SectionMap } from "../components/SectionMap";
+import { HeaderAbout } from "../components/HeaderAbout";
 
 export const AboutPage = () => {
   return (
-    <> 
-    <div >AboutPage</div>
+    <>
+      <main>
+        <HeaderAbout />
+        <section className="pb-20 bg-gray-200 -mt-24">
+          <div className="container mx-auto px-4 lg:px-20">
+            <SectionMap />
+          </div>
+        </section>
+        </main> 
     </>
-  )
-}
+  );
+};
