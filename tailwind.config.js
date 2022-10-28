0af519aa05f9f5fb43ae724ecb47c2fe3c764761
ZonaGamerSogamoso <116860278+ZonaGamerSogamoso@users.noreply.github.com>
2022-10-27 23:23:55 -0500

    se añadieron algunos estilos personalizados a tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+ theme: {
+  extend: {
+    inset: {
+      "-100": "-100%",
+      "-225-px": "-225px",
+      "-160-px": "-160px",
+      "-150-px": "-150px",
+      "-94-px": "-94px",
+      "-50-px": "-50px",
+      "-29-px": "-29px",
+      "-20-px": "-20px",
+      "25-px": "25px",
+      "40-px": "40px",
+      "95-px": "95px",
+      "145-px": "145px",
+      "195-px": "195px",
+      "210-px": "210px",
+      "260-px": "260px",
+    },
+    height: {
+      "95-px": "95px",
+      "70-px": "70px",
+      "350-px": "350px",
+      "500-px": "500px",
+      "600-px": "600px",
+    },
+  },
+  },
+  plugins: [],
+};
