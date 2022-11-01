8dd902cf7744c95bf8fe4f024a95383d57a7781f
Andrés <sergio.cruz02@uptc.edu.co>
2022-10-31 22:18:58 -0500

    Header about modified

--- src/pages/About/components/HeaderAbout.jsx
+++ src/pages/About/components/HeaderAbout.jsx
@@ -20,6 +20,9 @@ export const HeaderAbout = () => {
                     <h1 className="text-white font-semibold text-5xl">
                       Zona Gamer
                     </h1>
+                    <p className="mt-4 text-lg text-gray-200">
+                      Aquí nos conocerás un poco más de nosotros
+                    </p>
                   </div>
                 </div>
               </div>
