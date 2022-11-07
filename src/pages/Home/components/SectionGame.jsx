d8a50c5cc8671882b08ec8e6bda393fc2fb47884
AstridCely <sonia.cely@uptc.edu.co>
2022-11-06 23:08:47 -0500

    correccion de errores y atributo className

--- src/pages/Home/components/SectionGame.jsx
+++ src/pages/Home/components/SectionGame.jsx
@@ -30,26 +30,26 @@ export const SectionGame = ({ listGames }) => {
 
   const getGame = (id) => {
     return (
-      <div class="p-4 md:w-1/3">
-        <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+      <div className="p-4 md:w-1/3">
+        <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
           <img
-            class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+            className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
             src={listGames.games[id].thumbnail}
             alt={listGames.games[id].title}
           />
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
               {listGames.games[id].genre}
             </h2>
-            <h1 class="title-font text-lg font-medium text-gray-600 mb-3">
+            <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
               {listGames.games[id].title}
             </h1>
-            <p class="leading-relaxed mb-3 h-32 overflow-hidden">
+            <p className="leading-relaxed mb-3 h-32 overflow-hidden">
               {listGames.games[id].short_description}
             </p>
-            <div class="flex items-center flex-wrap ">
+            <div className="flex items-center flex-wrap ">
               <button
-                class="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg"
+                className="bg-gradient-to-r from-cyan-400 to-blue-400 hover:scale-105 drop-shadow-md  shadow-cla-blue px-4 py-1 rounded-lg"
                 onClick={() => {
                   window.open(listGames.games[id].game_url, "_blank");
                 }}
@@ -65,12 +65,12 @@ export const SectionGame = ({ listGames }) => {
 
   if (listGames.isLoading) return;
   return (
-    <section class="text-gray-600 body-font bg-gray-300/30">
-      <div class="container px-5 py-7 mx-auto">
-        <h1 class="text-[30px] lg:text-[25px] xl:text-[30px] font-bold leading-tight mt-5 text-center pb-10 sm:mt-0 underline">
+    <section className="text-gray-600 body-font bg-gray-300/30">
+      <div className="container px-5 py-7 mx-auto">
+        <h1 className="text-[30px] lg:text-[25px] xl:text-[30px] font-bold leading-tight mt-5 text-center pb-10 sm:mt-0 underline">
           Videojuegos
         </h1>
-        <div class="flex flex-wrap -m-4">
+        <div className="flex flex-wrap -m-4">
           {getGame(pagination)}
           {getGame(pagination - 1)}
           {getGame(pagination - 2)}
@@ -78,19 +78,19 @@ export const SectionGame = ({ listGames }) => {
           {getGame(pagination - 4)}
           {getGame(pagination - 5)}
         </div>
-        <div class="flex items-center justify-center my-4">
-          <div class="flex justify-center items-center ">
+        <div className="flex items-center justify-center my-4">
+          <div className="flex justify-center items-center ">
             <div
-              class="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+              className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
               onClick={() => onChangePagination("left")}
             >
               &#60;
             </div>
-            <div class="text-slate-500">
+            <div className="text-slate-500">
               {page} / {Math.trunc(listGames.games.length / 6)}
             </div>
             <div
-              class="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+              className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
               onClick={() => onChangePagination("right")}
             >
               &#62;
--- src/pages/Home/components/SectionNews.jsx
+++ src/pages/Home/components/SectionNews.jsx
@@ -36,29 +36,29 @@ const SectionNews = ({ listNews, listGiveaways }) => {
 
   const getGiveaway = (id) => {
     return (
-      <div class="p-1 md:w-full h-1/3">
-        <div class="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+      <div className="p-1 md:w-full h-1/3">
+        <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
           <img
-            class="lg:h-30 md:h-30 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-105 cursor-pointer"
+            className="lg:h-30 md:h-30 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-105 cursor-pointer"
             onClick={() => {
                 window.open(listGiveaways.giveaways[id].giveaway_url, "_blank");
               }}
             src={listGiveaways.giveaways[id].main_image}
             alt={listGiveaways.giveaways[id].title}
           />
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-              <span class="bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+              <span className="bg-purple-100 text-purple-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-200 dark:text-purple-900">
                 Regalo
               </span>
             </h2>
-            <h1 class="title-font text-lg font-medium text-gray-600 mb-3">
+            <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
               {listGiveaways.giveaways[id].title}
             </h1>
-            <p class="leading-relaxed mb-3 h-28 overflow-hidden">
+            <p className="leading-relaxed mb-3 h-28 overflow-hidden">
               {listGiveaways.giveaways[id].short_description}
             </p>
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
             {listGiveaways.giveaways[id].keys_left}
             </h2>
           </div>
@@ -69,33 +69,33 @@ const SectionNews = ({ listNews, listGiveaways }) => {
 
   const getNew = (id) => {
     return (
-      <div class="p-4 md:w-1/3">
-        <div class="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden">
+      <div className="p-4 md:w-1/3">
+        <div className="h-full rounded-xl shadow-cla-violate bg-gradient-to-r from-pink-50 to-red-50 overflow-hidden">
           <img
-            class="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+            className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
             src={listNews.news[id].main_image}
             alt={listNews.news[id].title}
           />
-          <div class="p-6">
-            <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-2">
-              <span class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+          <div className="p-6">
+            <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-2">
+              <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
                 Noticia
               </span>
             </h2>
-            <h1 class="title-font text-lg font-medium text-gray-600 mb-3">
+            <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
               {listNews.news[id].title}
             </h1>
-            <p class="leading-relaxed mb-3 h-32 overflow-hidden">
+            <p className="leading-relaxed mb-3 h-32 overflow-hidden">
               {listNews.news[id].short_description}
             </p>
-            <div class="flex items-center flex-wrap ">
+            <div className="flex items-center flex-wrap ">
               <button
-                class="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg"
+                className="bg-gradient-to-r from-orange-300 to-amber-400 hover:scale-105 drop-shadow-md shadow-cla-violate px-4 py-1 rounded-lg"
                 onClick={() => {
                   window.open(listNews.news[id].article_url, "_blank");
                 }}
               >
-                Learn more
+                Ver mas
               </button>
             </div>
           </div>
@@ -105,9 +105,9 @@ const SectionNews = ({ listNews, listGiveaways }) => {
   };
 
   return (
-    <div class="flex mb-4">
-      <div class="w-3/4 bg-gray-300 flex flex-wrap text-gray-600">
-        <h1 class="text-[30px] lg:text-[25px] xl:text-[30px] font-bold leading-tight mt-5 text-center py-5 sm:mt-0 underline w-full">
+    <div className="flex mb-4">
+      <div className="w-3/4 bg-gray-300 flex flex-wrap text-gray-600">
+        <h1 className="text-[30px] lg:text-[25px] xl:text-[30px] font-bold leading-tight mt-5 text-center py-5 sm:mt-0 underline w-full">
           Noticias
         </h1>
         {getNew(pagination)}
@@ -116,19 +116,19 @@ const SectionNews = ({ listNews, listGiveaways }) => {
         {getNew(pagination - 3)}
         {getNew(pagination - 4)}
         {getNew(pagination - 5)}
-        <div class="flex items-center justify-center my-4 w-full">
-          <div class="flex justify-center items-center ">
+        <div className="flex items-center justify-center my-4 w-full">
+          <div className="flex justify-center items-center ">
             <div
-              class="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+              className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
               onClick={() => onChangePagination("left")}
             >
               &#60;
             </div>
-            <div class="text-slate-500">
+            <div className="text-slate-500">
               {page} / {Math.trunc(listNews.news.length / 6)}
             </div>
             <div
-              class="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
+              className="border rounded-md bg-gray-100 px-2 py-1 text-3xl leading-6 text-slate-400 transition hover:bg-gray-200 hover:text-slate-500 cursor-pointer shadow-sm"
               onClick={() => onChangePagination("right")}
             >
               &#62;
@@ -136,7 +136,7 @@ const SectionNews = ({ listNews, listGiveaways }) => {
           </div>
         </div>
       </div>
-      <div class="w-1/4 bg-gray-400/10">
+      <div className="w-1/4 bg-gray-400/10">
         {getGiveaway(0)}
         {getGiveaway(1)}
         {getGiveaway(2)}
