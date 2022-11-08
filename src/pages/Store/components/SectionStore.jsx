11dd304422d7eb508e62ec6a1d0cc4eb52255e50
Andrés <sergio.cruz02@uptc.edu.co>
2022-11-08 00:17:46 -0500

    Page Store section items

--- src/pages/Store/components/SectionStore.jsx
+++ src/pages/Store/components/SectionStore.jsx
@@ -3,7 +3,7 @@ export const SectionStore = () => {
         <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
           <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
           <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                    src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/giftShop.jpg" class="max-w-full h-auto" alt="..." />
+                    src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" class="max-w-full h-auto" alt="..." />
             <div className="p-6">
               <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
                 Descripción
