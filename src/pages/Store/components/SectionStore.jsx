d9f06defaf34f013787eb12cdd60f542d223e89c
Andrés <sergio.cruz02@uptc.edu.co>
2022-11-08 00:42:41 -0500

    Section Store

--- src/pages/Store/components/SectionStore.jsx
+++ src/pages/Store/components/SectionStore.jsx
@@ -1,24 +1,113 @@
 export const SectionStore = () => {
     return (
-        <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
-          <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
-          <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
-                    src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" class="max-w-full h-auto" alt="..." />
-            <div className="p-6">
-              <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                Descripción
-              </h2>
-              <h1 className="title-font text-lg font-medium text-gray-600 mb-3">
-                Nombre
-              </h1>
-              <p className="leading-relaxed mb-3 h-32 overflow-hidden">
-                Desc
-              </p>
-              <div className="flex items-center flex-wrap ">
-                  Ver más
-              </div>
+        <div className="flex flex-wrap">
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
+
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="lg:pt-28 p-6 md:w-1/3 sm:w-1/2">
+                <div className="h-full rounded-xl shadow-cla-blue bg-gradient-to-r from-indigo-50 to-blue-50 overflow-hidden">
+                    <img className="lg:h-48 md:h-36 w-full object-cover object-center scale-110 transition-all duration-400 hover:scale-100"
+                        src="https://raw.githubusercontent.com/ZonaGamerSogamoso/ZonaGamerPage/main/src/assets/img/Funko.png" />
+                    <div className="p-6">
+                         <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                            Descripción
+                        </h2>
+                        <h1 className="title-font text-lg font-bold text-gray-600 mb-3">
+                        Funko Pop
+                        </h1>
+                        <p className="leading-relaxed overflow-hidden">
+                        Funko diseña y vende coleccionables, accesorios y juguetes únicos de la cultura pop.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
