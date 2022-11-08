2f3dc1529df9780f8d8eb509aca7fe6db96af8c6
AstridCely <sonia.cely@uptc.edu.co>
2022-11-07 23:47:12 -0500

    implementacion useForm en formulario about

--- src/pages/About/components/SectionMap.jsx
+++ src/pages/About/components/SectionMap.jsx
@@ -1,4 +1,16 @@
+import { useForm } from "../../../hooks/useForm";
+
 export const SectionMap = () => {
+  const { email, message, onInputChange } = useForm({
+    email: "",
+    message: "",
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log({ email, message });
+    //Todo: enviar Formulario al backend
+  };
   return (
     <div className="flex flex-wrap">
       <div className="lg:pt-24 pt-6 w-full md:w-4/12 px-4 text-center">
@@ -35,7 +47,9 @@ export const SectionMap = () => {
             </div>
             <h6 className="text-xl font-semibold">Horarios</h6>
             <p className="mt-2 mb-4 text-blueGray-500">
-              Lunes a viernes: 8 a.m. a 8 p.m.<br/>Sabados y domingos: 10 a.m. a 8 p.m.
+              Lunes a viernes: 8 a.m. a 8 p.m.
+              <br />
+              Sabados y domingos: 10 a.m. a 8 p.m.
             </p>
           </div>
         </div>
@@ -55,36 +69,51 @@ export const SectionMap = () => {
         </div>
         <div className="container px-5 py-24 mx-auto flex">
           <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Contacto
-            </h2>
-            <p className="leading-relaxed mb-5 text-gray-600">
-              Si tienes alguna duda contacta con nosotros
-            </p>
-            <div className="relative mb-4">
-              <label htmlFor="email" className="leading-7 text-sm text-gray-600">
-                Correo Electronico
-              </label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-              />
-            </div>
-            <div className="relative mb-4">
-              <label htmlFor="message" className="leading-7 text-sm text-gray-600">
-                Mensaje
-              </label>
-              <textarea
-                id="message"
-                name="message"
-                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
-              ></textarea>
-            </div>
-            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Enviar
-            </button>
+            <form onSubmit={onSubmit}>
+              <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+                Contacto
+              </h2>
+              <p className="leading-relaxed mb-5 text-gray-600">
+                Si tienes alguna duda contacta con nosotros
+              </p>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="email"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Correo Electronico
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={onInputChange}
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                />
+              </div>
+              <div className="relative mb-4">
+                <label
+                  htmlFor="message"
+                  className="leading-7 text-sm text-gray-600"
+                >
+                  Mensaje
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={onInputChange}
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+              >
+                Enviar
+              </button>
+            </form>
           </div>
         </div>
       </section>
@@ -97,7 +126,7 @@ export const SectionMap = () => {
             <h6 className="text-xl font-semibold">Contacto y Ubicación</h6>
             <p className="mt-2 mb-4 text-blueGray-500">
               Tambien nos puedes encontrar en el siguiente correo:
-              <br/>
+              <br />
               <span className="font-bold">zonagamersogamoso@gmail.com</span>
             </p>
           </div>
